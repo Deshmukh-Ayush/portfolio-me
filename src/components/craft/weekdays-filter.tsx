ab30eb171c919fa0e36dc8b33358ec5f1f6d8f2b
Ayush <ayushdeshmukh301@gmail.com>
2026-08-18 02:57:41 +0530

    fix: update styles for weekdays filter component to improve dark mode support

--- src/components/craft/weekdays-filter.tsx
+++ src/components/craft/weekdays-filter.tsx
@@ -28,8 +28,8 @@ export const WeekdaysFilter = () => {
   };
 
   return (
-    <div className="shadow-border h-44 w-74 rounded-xl bg-neutral-50 p-2 text-neutral-900">
-      <div className="shadow-border-sm flex h-full w-full flex-col items-center justify-center rounded-lg bg-white p-3">
+    <div className="shadow-border h-44 w-74 rounded-xl bg-neutral-50 p-2 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100">
+      <div className="shadow-border-sm flex h-full w-full flex-col items-center justify-center rounded-lg bg-white p-3 dark:bg-neutral-950">
         <div className="flex h-10 w-full items-center justify-between">
           <span>Is Repeating</span>
 
@@ -42,19 +42,19 @@ export const WeekdaysFilter = () => {
             />
 
             {/* Track */}
-            <div className="shadow-border-sm h-4.5 w-8 rounded-full bg-neutral-200/80 transition duration-300 ease-in-out peer-checked:bg-green-500 peer-focus-visible:ring-1 peer-focus-visible:ring-neutral-400/50 peer-focus-visible:ring-offset-1 peer-disabled:cursor-not-allowed peer-disabled:opacity-40 dark:bg-green-500 dark:peer-checked:bg-neutral-100 dark:peer-focus-visible:ring-offset-green-600" />
+            <div className="shadow-border-sm h-4.5 w-8 rounded-full bg-neutral-200/80 transition duration-300 ease-in-out peer-checked:bg-green-500 peer-focus-visible:ring-1 peer-focus-visible:ring-neutral-400/50 peer-focus-visible:ring-offset-1 peer-disabled:cursor-not-allowed peer-disabled:opacity-40 dark:bg-neutral-800 dark:peer-checked:bg-green-500 dark:peer-focus-visible:ring-neutral-500/50 dark:peer-focus-visible:ring-offset-neutral-950" />
 
             {/* Thumb */}
-            <div className="shadow-border-sm absolute top-[3px] left-[3px] h-3 w-3 rounded-full bg-white transition-all duration-150 ease-in-out peer-checked:translate-x-3.5 dark:bg-neutral-950" />
+            <div className="shadow-border-sm absolute top-[3px] left-[3px] h-3 w-3 rounded-full bg-white transition-all duration-150 ease-in-out peer-checked:translate-x-3.5 dark:bg-neutral-100" />
           </label>
         </div>
 
         <div className="mb-2 flex h-17 w-full items-end justify-between gap-2 py-1">
-          <button className="mb-0.5 cursor-pointer rounded-2xl border-neutral-300 px-3 py-1 text-sm">
+          <button className="mb-0.5 cursor-pointer rounded-2xl border-neutral-300 px-3 py-1 text-sm dark:border-neutral-700 dark:text-neutral-200">
             Repeat
           </button>
 
-          <button className="flex w-full cursor-pointer items-center justify-between rounded-lg border border-neutral-200 px-3 py-1 text-sm">
+          <button className="flex w-full cursor-pointer items-center justify-between rounded-lg border border-neutral-200 px-3 py-1 text-sm dark:border-neutral-800 dark:bg-neutral-950 dark:text-neutral-200">
             <span>Weekly</span>
             <HugeiconsIcon icon={ArrowDown01Icon} />
           </button>
@@ -94,12 +94,12 @@ const WeekFIlter = ({ isRepeating, markedDays }: WeekFilterProps) => {
               }}
             >
               {/* OFF */}
-              <div className="flex h-8 w-8 shrink-0 items-center justify-center rounded-md text-[10px]">
+              <div className="flex h-8 w-8 shrink-0 items-center justify-center rounded-md text-[10px] text-neutral-700 dark:text-neutral-400">
                 {day}
               </div>
 
               {/* ON */}
-              <div className="flex h-8 w-8 shrink-0 items-center justify-center rounded-md border border-neutral-200 bg-white text-[10px]">
+              <div className="flex h-8 w-8 shrink-0 items-center justify-center rounded-md border border-neutral-200 bg-white text-[10px] text-neutral-900 dark:border-neutral-800 dark:bg-neutral-900 dark:text-neutral-100">
                 {day}
               </div>
             </motion.div>
